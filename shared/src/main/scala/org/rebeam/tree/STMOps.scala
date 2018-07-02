980697d72f6d5ca4ce79c3456b643477c63e248f
trepidacious <trepidacious@gmail.com>
2018-07-02 22:49:00 +0100

    Try to add STMOps syntax again, nearly works

--- shared/src/main/scala/org/rebeam/tree/STMOps.scala
+++ shared/src/main/scala/org/rebeam/tree/STMOps.scala
@@ -6,15 +6,40 @@ object STMOps {
 
 // TODO there might be some way to provide these functions to enable one import STM._ rather than
 // an import stm._ in every program, but the [A] makes it difficult.
-//  def get[A, F[_]](id: Id[A])(implicit stm: STM[F]): F[Option[A]] = stm.get(id)
-//  def set[A, F[_]](id: Id[A], a: A)(implicit stm: STM[F]): F[Unit] = stm.set(id, a)
+
+//  case class Getter[A]() {
+//    def apply[F[_]](id: Id[A])(implicit stm: STMOps[F]): F[Option[A]] = stm.get(id)
+//  }
 //
-//  def randomInt[F[_]](implicit stm: STM[F]): F[Int] = stm.randomInt
-//  def randomIntUntil[F[_]](bound: Int)(implicit stm: STM[F]): F[Int] = stm.randomIntUntil(bound)
-//  def randomLong[F[_]](implicit stm: STM[F]): F[Long] = stm.randomLong
-//  def randomBoolean[F[_]](implicit stm: STM[F]): F[Boolean] = stm.randomBoolean
-//  def randomFloat[F[_]](implicit stm: STM[F]): F[Float] = stm.randomFloat
-//  def randomDouble[F[_]](implicit stm: STM[F]): F[Double] = stm.randomDouble
+//  case class Setter[A]() {
+//    def apply[F[_]](id: Id[A], a: A)(implicit stm: STMOps[F]): F[Unit] = stm.set(id, a)
+//  }
+//
+//  case class PutterF[A]() {
+//    def apply[F[_]](create: Id[A] => F[A])(implicit stm: STMOps[F]): F[A] = stm.putF(create)
+//  }
+//
+//  case class Putter[A]() {
+//    def apply[F[_]](create: Id[A] => A)(implicit stm: STMOps[F]): F[A] = stm.put(create)
+//  }
+//
+//  def set[A]: Setter[A] = Setter[A]()
+//
+//  def putF[A]: PutterF[A] = PutterF[A]()
+//
+//  def put[A]: Putter[A] = Putter[A]()
+//
+//  def get[A]: Getter[A] = Getter[A]()
+//
+//  //  def get[A, F[_]](id: Id[A])(implicit stm: STMOps[F]): F[Option[A]] = stm.get(id)
+//  //  def set[A, F[_]](id: Id[A], a: A)(implicit stm: STMOps[F]): F[Unit] = stm.set(id, a)
+////
+//  def randomInt[F[_]](implicit stm: STMOps[F]): F[Int] = stm.randomInt
+//  def randomIntUntil[F[_]](bound: Int)(implicit stm: STMOps[F]): F[Int] = stm.randomIntUntil(bound)
+//  def randomLong[F[_]](implicit stm: STMOps[F]): F[Long] = stm.randomLong
+//  def randomBoolean[F[_]](implicit stm: STMOps[F]): F[Boolean] = stm.randomBoolean
+//  def randomFloat[F[_]](implicit stm: STMOps[F]): F[Float] = stm.randomFloat
+//  def randomDouble[F[_]](implicit stm: STMOps[F]): F[Double] = stm.randomDouble
 
 }
 
@@ -26,7 +51,6 @@ abstract class STMOps[F[_]: Monad] extends TransactionOps {
   def set[A](id: Id[A], a: A): F[Unit]
 
   def modifyF[A](id: Id[A], f: A => F[A]): F[Option[A]]
-
 
   //Safer if this is used only via put and putF
   //def createGuid: F[Guid]
